cd4d56ff908b34497a9d45ac4be123d49bd24c2a
Shofiqur Rahman <shofi384@gmail.com>
2018-10-06 11:19:08 -0400

    adding opn to open the app on browser

--- QuizBase/main.js
+++ QuizBase/main.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const opn = require('opn');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// opens the url in the default browser 
+opn('http://localhost:3000/');
