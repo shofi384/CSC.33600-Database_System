befd273452bdd1a018bc4d2405d36b806ce8a4c2
Shofiqur Rahman <shofi384@gmail.com>
2018-10-07 16:55:30 -0400

    Adding index.html and express functionality to return the page

--- QuizBase/main.js
+++ QuizBase/main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const opn = require('opn');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }));	// __dirname returns the directory that the currently executing script is in
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
